cbad042156fec6918765ec0cdf6218da67b95735
desqui92 <desqui92@gmail.com>
2020-03-09 18:12:27 -0300

    arreglo

--- menu-web.php
+++ menu-web.php
@@ -4,34 +4,39 @@
 <i class="fas fa-bars"></i>
   </button>
 <div class="collapse navbar-collapse" id="navbarTogglerDemo01" style="text-align:center;">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item" id="inicio1">
+    <ul class="navbar-nav mr-auto margenabajo">
+        <li class="nav-item margenabajo" id="inicio1">
             <a class="link1 text-muted spancito" href="" style="font-family:Montserrat;">Inicio</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item margenabajo" id="quienesomos">
           <a class="link1 text-muted spancito" href="#" style="font-family:Montserrat;">Quienes Somos</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item margenabajo" id="cursos">
         <a class="link1 text-muted spancito"href="#" style="font-family:Montserrat;">Cursos</a>
       </li>
-        <li class="nav-item">
+        <li class="nav-item margenabajo">
         <a class="link1 text-muted spancito"href="#" style="font-family:Montserrat;">Blog</a>
       </li>
     </ul>
-      <div style="margin-right:15px;" class="d-flex justify-content-center"><form class="form-inline my-2 my-lg-0">
-<div class="input-group mb-3" style="margin-right:10px; margin-top:15px;">
+    <div style="margin-right:15px;" class="d-flex justify-content-center">
+      <form class="form-inline my-2 my-lg-0">
+      <div class="input-group mb-3" style="margin-right:10px; margin-top:15px;">
  <!-- <div class="input-group-prepend">
     <span class="input-group-text" id="basic-addon1" style="background-color:red;"><i class="fa fa-search" style="color:white;"></i></span>
   </div>
   <input type="text" class="form-control" placeholder="Buscar..." aria-label="Username" aria-describedby="basic-addon1" style="font-size:15px;font-family:opensans">
 </div>-->
-          </form></div>
-    <div style="margin-right:15px;">   <div style="margin-right:15px; margin-bottom:5px; margin-top:5px;">
-    <i class="fa fa-envelope text-danger"></i><span class="text-muted">&nbsp;info@idemsalud.com </span>
-    <i class="fa fa-phone text-danger"></i><span class="text-muted">&nbsp;0221-4707735</span>
-  </div>
-  <button type="button" class="btn btn-op1"><i class="far fa-user-circle fa-lg"></i> Entrar / Registrarse</button> </div>
-</div></div>
+       </form>
+       </div>
+    <div style="margin-right:15px;">   
+      <div style="margin-right:15px; margin-bottom:5px; margin-top:5px;">
+        <i class="fa fa-envelope text-danger"></i><span class="text-muted">&nbsp;info@idemsalud.com </span>
+        <i class="fa fa-phone text-danger"></i><span class="text-muted">&nbsp;0221-4707735</span>
+      </div>
+      <button type="button" class="btn btn-op1"><i class="far fa-user-circle fa-lg"></i> Entrar / Registrarse</button>
+    </div>
+    </div>
+</div>
    <!-- <div style="margin-right:15px;"><button type="button" class="btn btn-outline-danger">Iniciar Sesión</button></div>
     <div style="margin-right:15px;"><button type="button" class="btn btn-outline-danger">Regístrate</button></div>-->
 </nav>
