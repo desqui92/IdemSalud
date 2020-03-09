8b1cc5f80e01e56a689aab840493eeee6708d9e3
Mariano_Flores <marianobc12@hotmail.com>
2020-03-09 17:12:37 -0300

    seccion inicio

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   ?>
   <div class="container-fluid h-100" id="inicio-seccion">
     <div class="row">
-      <div class="col">
+      <div class="col-6 cont-inicio">
           <h1 class="titulo">¡Mira nuestros cursos!</h1>
           <h4 class="descripcion">Ahora te podes anotar a nuestros cursos de enfermería</h4>
       </div>
