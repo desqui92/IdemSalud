7617bf99d1899fbcd025efd7a553ef684b04f89f
Mariano_Flores <marianobc12@hotmail.com>
2020-03-09 15:55:12 -0300

    sacar link de fuente

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/estilo.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
     <title>Inicio</title>
 </head>
 <body>
