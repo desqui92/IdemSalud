bc5d1fab2c2ca5eeb27bedc52faa9ea6bb99a78f
Mariano_Flores <marianobc12@hotmail.com>
2020-03-09 15:03:29 -0300

    arreglo menu

--- index.php
+++ index.php
@@ -10,49 +10,9 @@
     <title>Inicio</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-xl" id="navid" style="border-style: solid; border-width:0px 0px 2px 0px;border-color:red; font-family:opensans; font-size:15px;">
-<a class="navbar-brand" href="#"><img src="img/logo.png"></a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-<i class="fas fa-bars"></i>
-  </button>
-<div class="collapse navbar-collapse" id="navbarTogglerDemo01" style="text-align:center;">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item" id="inicio1">
-            <a class="link1 text-muted spancito" href="">Inicio</a>
-      </li>
-        <li class="nav-item dropdown" id="cursos1">
-<a class="dropdown-toggle link1 text-muted spancito" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Cursos
-        </a>
-        <div class="dropdown-menu" id="cajasubmenu" aria-labelledby="navbarDropdown" style="border-radius:0px !important; text-align:center !important; max-width:200px;">
-          <a class="dropdown-item link1 text-muted" href="#">Curso1</a>
-          <a class="dropdown-item link1 text-muted" href="#">Curso2</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item link1 text-muted" href="#">Curso3</a>
-        </div>
-      </li>
-        <li class="nav-item">
-        <a class="link1 text-muted spancito"href="#">Blog</a>
-      </li>
-      <li class="nav-item">
-          <a class="link1 text-muted spancito" href="#">Quienes Somos</a>
-      </li>
-    </ul>
-      <div style="margin-right:15px;" class="d-flex justify-content-center"><form class="form-inline my-2 my-lg-0">
-<div class="input-group mb-3" style="margin-right:10px; margin-top:15px;">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon1" style="background-color:red;"><i class="fa fa-search" style="color:white;"></i></span>
-  </div>
-  <input type="text" class="form-control" placeholder="Buscar..." aria-label="Username" aria-describedby="basic-addon1" style="font-size:15px;font-family:opensans">
-</div>
-          </form></div>
-    <div style="margin-right:15px;"><i class="fa fa-envelope text-danger"></i><span class="text-muted">&nbsp;info@idemsalud.com</span></div>
-    <div style="margin-right:15px;"><i class="fa fa-phone text-danger"></i><span class="text-muted">&nbsp;0221-4707735</span></div>
-    <div style="margin-right:15px;"><i class="fas fa-shopping-cart text-danger"></i></div>
-    </div>
-   <!-- <div style="margin-right:15px;"><button type="button" class="btn btn-outline-danger">Iniciar Sesión</button></div>
-    <div style="margin-right:15px;"><button type="button" class="btn btn-outline-danger">Regístrate</button></div>-->
-</nav>
+<?php
+  include 'menu-web.php';
+?>
 <div class="imagenenf"><img src="img/enfermera.jpg" class="fondo1"></div>
 <div class="curso"><h1>Curso</h1></div>
 
