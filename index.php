548a1cb28b9b843a15a672edb289bd97d5dc31cb
desqui92 <desqui92@gmail.com>
2020-03-10 14:37:57 -0300

    klklk

--- index.php
+++ index.php
@@ -13,24 +13,27 @@
   <?php
     include 'menu-web.php';
   ?>
-  <div class="container-fluid" id="inicio-seccion">
+  <div class="container-fluid" id="inicio-seccion" style="position:relative;">
     <div class="row">
+      <div clas="col-12">
          <img src="img/enfermera.jpg" style="width:100%; height:auto;">
+      </div>
       <div class="col-6 cont-inicio animated fadeInLeft slow" style="position:absolute;">
           <h1 class="titulo">¡MIRA NUESTROS CURSOS!</h1>
           <h4 class="descripcion">Ahora te podes anotar a nuestros cursos de enfermería</h4>
       </div>
     </div>
   </div>
-  <div class="container-fluid" id="info-seccion">
+  <div class="container-fluid animated fadeInLeft slow" id="info-seccion">
     <div class="row cont-info">
-      <div class="col-6">
-          <img src="img/worldwideweb.jpg" class="imagenformavirtual">
+      <div class="col-6" style="padding:0px;">
+      <img src="img/enfermeracelu.png" style="width:100%; height:auto;">
       </div>
-      <div class="col-6 animated fadeInLeft slow">
+      <div class="col-6" style="padding-top:3%; padding-bottom:3%; background-color:purple; text-align:center;">
           <h1 class="titulo2">¡REALIZA LOS CURSOS DE FORMA VIRTUAL!</h1>
           <h4 class="descripcion2">Desde tu casa a travez de tu computadora<br>
            o celular de forma 100% online.</h4>
+          <img src="img/global-education.png" class="imagenformavirtual img-fluid">
       </div>
     </div>
   </div>
