533ea0e74bef0529b21ebc0b34455fdd3540ae54
Mariano_Flores <marianobc12@hotmail.com>
2020-03-09 21:15:29 -0300

    nosotros

--- nosotros.php
+++ nosotros.php
@@ -12,6 +12,36 @@
     <?php
         include 'menu-web.php';
     ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col cont-nosotros">
+                <h1 class="mt-5 titulo animated fadeInLeft slower">¿QUIÉNES SOMOS?</h1>
+                <div class="row">
+                    <div class="col-6 p-5 animated fadeInLeft slower">
+                        <p>
+                        Un grupo de enfermeros avocados a la tarea de conseguir un espacio que satisfaga las necesidades que día a día van surgiendo como consecuencia de la interacción con su medio ambiente. Embarcados en esta meta y con el apadrinamiento de la Editorial IDEM, es que nace este portal de enfermería, desde Argentina para toda Latinoamérica. Un sitio que consagra al personal de enfermería en su afán de servicio al cuidado de la salud, con el esmero y dedicación del cual son portadores. Un espacio brindado para los profesionales de enfermería y aquellos estudiantes, futuros enfermeros que ansían abrazar esta carrera.
+                        </p>
+                        <p>
+                        Queremos lograr en el portal, un lugar: Donde se congregue la profesión de enfermería de toda Latinoamérica y se fomente la integración con sus colegas. Donde puedan interactuar entre sí y enriquecer conocimientos…
+                        </p>
+                        <p>…”Un lugar; donde estrechar lazos con enfermeros de otras naciones”</p>
+                        <p>“Enfermeros… de hoy y del mañana” Son la “esencia y ser” de este sitio, por lo que nos complace invitarlos a participar del mismo y sumarse a ser parte de el, haciendo de éste Portal un lugar propio donde su colaboración y aporte serán de gran importancia.</p>
+                        <p>Este aporte se puede dar a través de:</p>
+                        <ul>
+                            <li>El envío de trabajos de investigación.</li>
+                            <li>La confección de notas o artículos de salud.</li>
+                            <li>Apuntes y monografías</li>
+                            <li>Proyectos y planes de salud</li>
+                            <li>Entre otros.</li>
+                        </ul>
+                    </div>
+                    <div class="col-6 animated fadeInUp slower">
+                        <img src="img/enfermeros-nosotros.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
