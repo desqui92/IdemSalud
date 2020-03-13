bc6e12e056bb95cfe6ee9194f415c938313ee256
Mariano_Flores <marianobc12@hotmail.com>
2020-03-13 16:46:53 -0300

    arreglo del titulo slide

--- index.php
+++ index.php
@@ -62,8 +62,8 @@
         <div id="nuestroscursos">
         <h1 class="text-center" style="font-size:36px; margin-bottom:5px;margin-top:60px;"><img style="vertical-align:sub;" src="img/cursos-pc.png" height="40" alt=""> Cursos mas demandados</h1>
         <h2 class="text-center" style="font-size:22px; font-weight:300; color:#6c757d;margin-bottom:18px;margin-top:20px;">Elige uno de nuestros cursos y empieza a aprender con un profesional calificado.</h2>
-      <div  style="border-bottom:1px #d0d0d0 solid; padding-bottom:8px;margin-top:60px;">
-        <a href="" style="padding-bottom:9px; border-bottom:2px #000000 solid"><h2 style="font-size:21px; display:inline;">Enfermería</h2></a>&nbsp;&nbsp;<a href=""><h2 style="font-size:21px; display:inline;">Otros</h2></a>
+      <div  style="padding-bottom:8px;margin-top:60px;">
+        <a href="" style="padding-bottom:9px;"><h2 style="font-size:21px; display:inline;">Enfermería</h2></a>&nbsp;&nbsp;<a href=""><h2 style="font-size:21px; display:inline;">Otros</h2></a>
       </div>
     </div>
   </div>
