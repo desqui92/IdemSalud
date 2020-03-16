a56a7067a235bbefdf33719edc143f75d53c6371
desqui92 <desqui92@gmail.com>
2020-03-16 19:46:52 -0300

    ll

--- misCursos.php
+++ misCursos.php
@@ -12,6 +12,74 @@
     <?php
         include 'menu-web.php';
     ?>
+
+    <div class="container-fluid" style="margin-top:60px;">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12 col-sm-12 col-xl-12">
+                <h1 class="text-center wow fadeInUp" style="text-align:center; margin-top:-20px; margin-bottom:10px;"><img style="vertical-align:sub;" src="img/cursos.png" height="40" alt="">Nuestros Profesores</h1>
+                <hr style="border-top: 2px solid red; width:60px; margin-bottom:40px;">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3-auto col-md-3-auto col-lg-3-auto col-sm-3-auto col-xl-3-auto mx-auto">
+                <div class="card" style="width: 18rem; border:none;">
+                    <img class="card-img-top" src="img/img-1.jpg" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mariano Flores</h5>
+                        <h5 class="card-subtitle" style="font-size:15px; color:#6c757d">Porf. en Programación</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3-auto col-md-3-auto col-lg-3-auto col-sm-3-auto col-xl-3-auto mx-auto">
+                <div class="card" style="width: 18rem; border:none;">
+                    <img class="card-img-top" src="img/img-2.jpg" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Walter Fernandez</h5>
+                        <h5 class="card-subtitle" style="font-size:15px; color:#6c757d">Porf. en Enfermería</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3-auto col-md-3-auto col-lg-3-auto col-sm-3-auto col-xl-3-auto mx-auto">
+                <div class="card" style="width: 18rem; border:none;">
+                    <img class="card-img-top" src="img/img-3.jpg" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Juan Dinatale</h5>
+                        <h5 class="card-subtitle" style="font-size:15px; color:#6c757d">Porf. en Enfermería</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3-auto col-md-3-auto col-lg-3-auto col-sm-3-auto col-xl-3-auto mx-auto">
+                <div class="card" style="width: 18rem; border:none;">
+                    <img class="card-img-top" src="img/img-4.jpg" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Lucía Castro</h5>
+                        <h5 class="card-subtitle" style="font-size:15px; color:#6c757d">Porf. en Enfermería</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="row">
+            <div class="col-12 col-md-12 col-lg-12 col-sm-12 col-xl-12">
+                <h1 class="text-center wow fadeInUp" style="text-align:center; margin-top:-20px; margin-bottom:10px;"><img style="vertical-align:sub;" src="img/cursos.png" height="40" alt="">Nuestros cursos</h1>
+                <hr style="border-top: 2px solid red; width:60px; margin-bottom:40px;">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+            <div class="col-12 col-md-12 col-lg-12 col-sm-12 col-xl-12">
+                <h1 class="text-center wow fadeInUp" style="text-align:center; margin-top:-20px; margin-bottom:10px;"><img style="vertical-align:sub;" src="img/cursos.png" height="40" alt="">Nuestros cursos</h1>
+                <hr style="border-top: 2px solid red; width:60px; margin-bottom:40px;">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
     <div class="container-fluid">
         <div class="row">
             <div class="col text-center" style="margin-top:10%;">
