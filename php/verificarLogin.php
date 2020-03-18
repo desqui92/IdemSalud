9f5d3085b8c298213f2df4219ce934a76ad2cb00
Mariano_Flores <marianobc12@hotmail.com>
2020-03-18 04:07:59 -0300

    go

--- php/verificarLogin.php
+++ php/verificarLogin.php
@@ -11,7 +11,7 @@ $mysqli=conexion();
 $email=$_POST['email'];
 $clave=$_POST['clave'];
 
-/*$email="pepe@hotmail.com";
+/*$email="pmariano@hotmail.com";
 $clave="pepe";*/
 
 /* Realizo la consulta */
@@ -22,35 +22,42 @@ $sql->execute();
 $res=$sql->get_result();
 $user=$res->fetch_assoc();
 
-/* Evaluar si encontró registro */
-if($user['estado']=='1'){
-    if(password_verify($clave,$user['clave'])){
-        $_SESSION['idUsuario']=$user['idUsuario'];
-        $_SESSION['dni']=$user['dni'];
-        $_SESSION['nomApe']=$user['nomApe'];
-        $_SESSION['fechaNac']=$user['fechaNac'];
-        $_SESSION['provincia']=$user['provincia'];
-        $_SESSION['ciudad']=$user['ciudad'];
-        $_SESSION['telefono']=$user['telefono'];
-        $_SESSION['email']=$user['email'];
-        $_SESSION['estado']=$user['estado'];
-        $_SESSION['rango']=$user['rango'];
-        $_SESSION['fotoPerfil']=$user['fotoPerfil'];
-    
-        /* Variable de sesion que indica que esta logueado */
-        $_SESSION['conectado']='true';
-    
-        echo "entras!! puto!";
-    
+
+if(mysqli_num_rows($res)==1){
+    /* Evaluar si encontró registro */
+    if($user['estado']=='1'){
+        if(password_verify($clave,$user['clave'])){
+            $_SESSION['idUsuario']=$user['idUsuario'];
+            $_SESSION['dni']=$user['dni'];
+            $_SESSION['nomApe']=$user['nomApe'];
+            $_SESSION['fechaNac']=$user['fechaNac'];
+            $_SESSION['provincia']=$user['provincia'];
+            $_SESSION['ciudad']=$user['ciudad'];
+            $_SESSION['telefono']=$user['telefono'];
+            $_SESSION['email']=$user['email'];
+            $_SESSION['estado']=$user['estado'];
+            $_SESSION['rango']=$user['rango'];
+            $_SESSION['fotoPerfil']=$user['fotoPerfil'];
+        
+            /* Variable de sesion que indica que esta logueado */
+            $_SESSION['conectado']='true';
+        
+            echo "entras!! puto!";
+        
+        }else{
+            /* Variable de sesion que indica que no esta logueado */
+            $_SESSION['conectado']='false';
+        
+            echo "le pifiaste algun dato conchudo";
+        }   
     }else{
-        /* Variable de sesion que indica que no esta logueado */
-        $_SESSION['conectado']='false';
-    
-        echo "le pifiaste algun dato conchudo";
-    }   
+        echo "pelotudo te olvidaste de validar el email";
+    }
 }else{
-    echo "pelotudo te olvidaste de validar el email";
+    echo "No existe la cuenta";
 }
+
+
 
 
 
